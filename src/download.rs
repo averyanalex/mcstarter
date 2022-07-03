72164c3c182f30b93430c1971801871676c3373d
Alexander Averyanov <alex@averyan.ru>
2022-07-04 00:49:57 +0300

    Remove dead code

--- src/download.rs
+++ src/download.rs
@@ -3,8 +3,6 @@ extern crate reqwest;
 use anyhow::Result;
 use bytes::Bytes;
 
-use std::fs;
-
 use crate::config::{Core, Plugin};
 use crate::hash::hash_bytes;
 
@@ -47,44 +45,5 @@ pub async fn download_hashed_core(core: &Core, expected_hash: &String) -> Result
         Ok(core_bytes)
     } else {
         todo!("return error core hash invalid")
-    }
-}
-
-pub async fn save_plugin(plugin: &Plugin, hash: &String, target: &String) -> Result<()> {
-    let name_version = plugin.name_version();
-    let save_path = format!("{target}/plugins/{name_version}.jar");
-
-    if !file_matchs_hash(&save_path, &hash) {
-        let plugin_bytes = download_hashed_plugin(plugin, &hash).await?;
-        fs::create_dir_all(format!("{target}/plugins"))?;
-        fs::write(save_path, &plugin_bytes)?;
-    }
-
-    Ok(())
-}
-
-pub async fn save_core(core: &Core, hash: &String, target: &String) -> Result<()> {
-    let save_path = format!("{target}/core.jar");
-
-    if !file_matchs_hash(&save_path, &hash) {
-        let core_bytes = download_hashed_core(core, &hash).await?;
-        fs::create_dir_all(target)?;
-        fs::write(save_path, &core_bytes)?;
-    }
-
-    Ok(())
-}
-
-fn file_matchs_hash(path: &String, hash: &String) -> bool {
-    let file = fs::read(path);
-    match file {
-        Ok(file_content) => {
-            if &hash_bytes(&Bytes::from(file_content)) == hash {
-                true
-            } else {
-                false
-            }
-        }
-        Err(_) => false,
     }
 }
