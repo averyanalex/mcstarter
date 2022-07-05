54b8204257d736610e0fb554704742a49fc8c3ba
Alexander Averyanov <alex@averyan.ru>
2022-07-05 18:41:19 +0300

    Arrays merging

--- src/merger.rs
+++ src/merger.rs
@@ -20,18 +20,33 @@ pub fn merge_yamls(a: &Yaml, b: &Yaml) -> Result<Yaml> {
 
 // Merge two YAML hashes
 fn merge_hashes(a: &Hash, b: &Hash) -> Hash {
-    let mut r = a.clone();
-    for (k, v) in b.iter() {
-        if let Yaml::Hash(bh) = v {
-            if let Entry::Occupied(e) = r.entry(k.clone()) {
-                if let Yaml::Hash(mut rh) = e.get().clone() {
-                    rh = merge_hashes(&rh, bh);
-                    r.insert(k.clone(), Yaml::Hash(rh));
+    let mut c = a.clone();
+    for (b_k, b_v) in b {
+        // If b_v is dict
+        if let Yaml::Hash(b_v_hash) = b_v {
+            // If b_k exists in c (originally a)
+            if let Entry::Occupied(e) = c.entry(b_k.clone()) {
+                // If c[b_k] also dict
+                if let Yaml::Hash(mut c_hash) = e.get().clone() {
+                    c_hash = merge_hashes(&c_hash, b_v_hash);
+                    c.insert(b_k.clone(), Yaml::Hash(c_hash));
                     continue;
                 }
             }
         }
-        r.insert(k.clone(), v.clone());
+        if b_v.is_array() {
+            if let Entry::Occupied(e) = c.entry(b_k.clone()) {
+                let cloned_e = e.get().clone();
+                if cloned_e.is_array() {
+                    let mut b_v_vector = b_v.as_vec().unwrap().clone();
+                    let mut vector = cloned_e.as_vec().unwrap().clone();
+                    vector.append(&mut b_v_vector);
+                    c.insert(b_k.clone(), Yaml::Array(vector));
+                    continue;
+                }
+            }
+        }
+        c.insert(b_k.clone(), b_v.clone());
     }
-    r
+    c
 }
