fb2cd2ae88b9eba73f834b99a05acdae11452107
averyanalex <alex@averyan.ru>
2022-07-28 14:18:23 +0300

    Nix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Minecraft server management utility";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        mcstarter = rustPlatform.buildRustPackage {
+          buildInputs = with pkgs; [ openssl ];
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          pname = "mcstarter";
+          version = "0.3.1";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      in
+      {
+        defaultPackage = mcstarter;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.stable.latest.default
+            pkg-config
+            openssl
+          ];
+        };
+      }
+    );
+}
